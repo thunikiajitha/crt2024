9fdc397dfc3c5c504d05673e8d5ecf3c178e1eb0
thunikiajitha <ajithathunikiajithathuniki@gmail.com>
2024-07-01 16:08:22 +0530

    Create compareTo()..java

---
+++ compareTo()..java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Test27
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+String s1=s.next();
+String s2=s.next();
+int r=s1.compareTo(s2);
+System.out.println(r);
+}
+}
