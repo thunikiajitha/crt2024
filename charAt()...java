ad059ab85187836d13ebb43429e6ac890b5e4da6
thunikiajitha <ajithathunikiajithathuniki@gmail.com>
2024-07-01 14:36:42 +0530

    Create charAt()...java

---
+++ charAt()...java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Test21
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+String s1=s.next();
+for(int i=0;i<s1.length();i++)
+{
+System.out.println(s1.charAt(i));
+}
+}
+}
