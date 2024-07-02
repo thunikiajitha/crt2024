de41ba36b3ad54762b8623fcabddce1742f21a1c
thunikiajitha <ajithathunikiajithathuniki@gmail.com>
2024-07-02 15:41:57 +0530

    Create start with,ends with,contains..java

---
+++ start with,ends with,contains..java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Test05
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+String str=("java is simple language");
+System.out.println(str.contains("simple"));
+System.out.println(str.contains("phython"));
+System.out.println(str.contains("lan"));
+System.out.println(str.contains("J"));
+System.out.println(str.contains("A"));
+System.out.println(str.contains("age"));
+}
+}
