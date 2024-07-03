6ebfac640b376b0926a073217e29ddbb34360fc5
thunikiajitha <ajithathunikiajithathuniki@gmail.com>
2024-07-03 13:56:27 +0530

    Create integerparser..java

---
+++ integerparser..java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Test012
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+String str1="234";
+String str2="563";
+System.out.println(Integer.parseInt(str1)+Integer.parseInt(str2));
+}
+}
