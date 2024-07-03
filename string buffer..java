178d004e1f33698c315a30dfe21bdb7f54614a5c
thunikiajitha <ajithathunikiajithathuniki@gmail.com>
2024-07-03 13:55:32 +0530

    Create string buffer..java

---
+++ string buffer..java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Test014
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+StringBuffer str1=new StringBuffer("java");
+StringBuffer str2=new StringBuffer("language");
+System.out.println(str1);
+System.out.println(str2);
+str1.append(str2);
+System.out.println(str1);
+}
+}
