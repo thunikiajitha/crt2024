21a10bf5257569c0b492a11410f9e383cf2fb7dd
thunikiajitha <ajithathunikiajithathuniki@gmail.com>
2024-07-03 13:35:54 +0530

    Create trim..java

---
+++ trim..java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Test011
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+String str="      java      lang     ";
+System.out.println(str.length());
+String str1=str.trim();
+System.out.println(str1.length());
+System.out.println(str1);
+}
+}
