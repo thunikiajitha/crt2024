e29db70c4545558461ee3b714df0ce24457ed421
thunikiajitha <ajithathunikiajithathuniki@gmail.com>
2024-07-04 14:42:07 +0530

    Create object of student...java

---
+++ object of student...java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Student
+{
+int admno;
+String name;
+public static void main(String args[])
+{
+Student s1=new Student();
+s1.admno=101;
+s1.name="sss";
+System.out.println(s1.admno+" "+s1.name);
+Student s2=new Student();
+s2.admno=105;
+s2.name="kkk";
+System.out.println(s2.admno+" "+s2.name);
+}
+}
