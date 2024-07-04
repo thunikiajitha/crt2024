c9f6ab5d453e656893bec7ab8c25d3ccd2ffa730
thunikiajitha <ajithathunikiajithathuniki@gmail.com>
2024-07-04 15:50:16 +0530

    Create instance variable..java

---
+++ instance variable..java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Sample
+{
+int a=10;
+}
+class TestA
+{
+public static void main(String args[])
+{
+Sample s1=new Sample();
+System.out.println(s1.a);
+Sample s2=new Sample();
+System.out.println(s2.a);
+}
+}
